3d185523f383d15e7c5c85026eecc207992a8d85
yukihane <yukihane.feather@gmail.com>
2017-12-31 16:04:37 +0900

    4.3 listに追加できるようにする

--- 04_01/app.js
+++ 04_01/app.js
@@ -3,5 +3,12 @@ new Vue({
     data: {
         todo: "",
         list: ["hoge", "fuga"]
+    },
+    methods: {
+        sendTodo: function() {
+            console.log("sendTodo: " + this.todo)
+            this.list.push(this.todo)
+            this.todo = ""
+        }
     }
 })
